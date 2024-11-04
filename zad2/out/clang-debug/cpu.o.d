119e8d2da4e9eda5a907aa1975b6f90901b0f979
mrudy4 <259581@student.pwr.edu.pl>
2024-11-04 19:37:37 +0100

    first commit

---
+++ zad2/out/clang-debug/cpu.o.d
@@ -0,0 +1,4 @@
+out/clang-debug//cpu.o: cpu.cc cpu.h \
+  /home/maciejrudy/Omnet++/omnetpp-6.1-linux-x86_64/omnetpp-6.1/include/omnetpp.h
+cpu.h:
+/home/maciejrudy/Omnet++/omnetpp-6.1-linux-x86_64/omnetpp-6.1/include/omnetpp.h:
---
+++ zad2/out/clang-debug/disk.o.d
@@ -0,0 +1,4 @@
+out/clang-debug//disk.o: disk.cc disk.h \
+  /home/maciejrudy/Omnet++/omnetpp-6.1-linux-x86_64/omnetpp-6.1/include/omnetpp.h
+disk.h:
+/home/maciejrudy/Omnet++/omnetpp-6.1-linux-x86_64/omnetpp-6.1/include/omnetpp.h:
